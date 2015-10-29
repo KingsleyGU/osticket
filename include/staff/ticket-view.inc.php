e904fe885a95f6255601a08fa891e7f1a2973f55
unknown <gumin12150454x@gmail.com>
2015-10-29 14:15:46 +0100

    add the click event to iframe

--- include/staff/ticket-view.inc.php
+++ include/staff/ticket-view.inc.php
@@ -882,7 +882,7 @@ print $note_form->getField('attachments')->render();
     <?php
     } ?>
     <form id="responza-Knowledge">
-        <iframe id="responza-Knowledge-frame" src="http://erst.spitzeco.dk/contentkb/1_1/1/1/Search?customer=Erhvervsstyrelse&title=1_1&page=1&sparam=<?php echo rawurlencode(Format::htmlchars($ticket->getSubject())); ?>" onhover="getContentofEmail(this)"></iframe>
+        <div id="responza-Knowledge-frame" data-src="http://erst.spitzeco.dk/contentkb/1_1/1/1/Search?customer=Erhvervsstyrelse&title=1_1&page=1&sparam=<?php echo rawurlencode(Format::htmlchars($ticket->getSubject())); ?>" onhover="getContentofEmail(this)"></div>
         <!-- <iframe src="http://www.baidu.com/"></iframe> -->
     </form>
 </div>
@@ -982,7 +982,7 @@ print $note_form->getField('attachments')->render();
 </div>
 <script type="text/javascript">
 $(document).ready(function(){
-    $("#responza-Knowledge-iframe").src = "http://www.baidu";
+    // $("#responza-Knowledge-iframe").src = "http://www.baidu";
 });
 $(function() {
    
