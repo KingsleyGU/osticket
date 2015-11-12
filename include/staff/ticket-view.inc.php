9d965cefb67d70b23ce183e66b9c392e228232f6
unknown <gumin12150454x@gmail.com>
2015-11-12 13:03:06 +0100

    change the tab to iframe again for demo

--- include/staff/ticket-view.inc.php
+++ include/staff/ticket-view.inc.php
@@ -1096,7 +1096,7 @@ $(".copyContentToClip").click(function(){
         return false;
 })
 $(".pasteContentFromClip").click(function(){
-    netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    unsafeWindow.netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");  
     var clipboardContent = getContentFromClipboard();
     alert(clipboardContent);
 })
