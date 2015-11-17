e5a7d558d5bd2a1c61f34a8b95977424cdb13cfa
unknown <gumin12150454x@gmail.com>
2015-11-17 13:26:07 +0100

    convert all the languages

--- scp/convertFile.php
+++ scp/convertFile.php
@@ -1,5 +1,6 @@
 <?php
 try {
+	echo INCLUDE_DIR.'/i18n/myphar.phar';
     $phar = new Phar(INCLUDE_DIR.'/i18n/myphar.phar');
     $phar->extractTo(INCLUDE_DIR.'/i18n/', 'lang.txt'); // extract only file.txt
 
