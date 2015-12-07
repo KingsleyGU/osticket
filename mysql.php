e843460abb4881b3cfbfa427c8faa52f623dfd35
unknown <gumin12150454x@gmail.com>
2015-12-07 14:24:40 +0100

    change the structure of table crm-subject-1

--- mysql.php
+++ mysql.php
@@ -2,23 +2,24 @@
 require('client.inc.php');
 // require_once(dirname(__file__).'/bootstrap.php');
 // include_once(INCLUDE_DIR.'mysqli.php');
-	changeTicketTable("orderNumber","string");
-	changeTicketTable("cvr_number","string");
+	// changeTicketTable("orderNumber","string");
 	// changeTicketTable("cvr_number","string");
-	changeTicketTable("company_name","string");
-	changeTicketTable("user_agent","string");
-	changeTicketTable("crm_subject1_id","int");
-	changeTicketTable("crm_subject2_id","int");
-	changeTicketTable("activity_id","int");
-	changeTicketTable("business_form_id","int");
+	// changeTicketTable("cvr_number","string");
+	// changeTicketTable("company_name","string");
+	// changeTicketTable("user_agent","string");
+	// changeTicketTable("crm_subject1_id","int");
+	// changeTicketTable("crm_subject2_id","int");
+	// changeTicketTable("activity_id","int");
+	// changeTicketTable("business_form_id","int");
 	addCRMSubject1();
 	addCRMSubject2();
-	addCRMActivity();
+	// addCRMActivity();
 	function addCRMSubject1()
 	{
 		$sql = 'CREATE TABLE ost_ticket_crm_subject1 (
 		id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 		crm_subject1_reference_id int(11) NOT NULL,
+		url VARCHAR(255),
 		crm_subject1_text text
 		)';
 		if(!($res=db_query($sql)) || !db_num_rows($res))
@@ -31,6 +32,9 @@ require('client.inc.php');
 		$sql = 'CREATE TABLE ost_ticket_crm_subject2 (
 		id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 		crm_subject2_reference_id int(11) NOT NULL,
+		cvr_rule VARCHAR(10),
+		order_rule VARCHAR(10),
+		title_rule VARCHAR(10),
 		crm_subject2_text text
 		)';
 		if(!($res=db_query($sql)) || !db_num_rows($res))
