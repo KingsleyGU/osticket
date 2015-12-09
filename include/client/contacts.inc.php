5070f62e1de77753bb656c3c4c9d4802ad5995fc
unknown <gumin12150454x@gmail.com>
2015-12-09 13:43:45 +0100

    change the web service error

--- include/client/contacts.inc.php
+++ include/client/contacts.inc.php
@@ -1,8 +1,11 @@
 <?php
 
-        $fileName = "https://w2l.dk/pls/wopdprod/erstcrm_pck.contact_xml";
-        $response = getRequestFromUrl($fileName);
-        if(!empty($response->xpath('/contacts/contact'))&&($nodes = $response->xpath('/contacts/contact'))&& count($nodes)>0)
-            createTicketByWebService($response);
+    // $fileName = CLIENTINC_DIR.'contact.xml';
+    $fileName = "https://w2l.dk/pls/wopdprod/erstcrm_pck.contact_xml";
+    $response = getRequestFromUrl($fileName);
+    if($response->xpath('/contacts/contact')&&!empty($response->xpath('/contacts/contact'))&&($nodes = $response->xpath('/contacts/contact'))&& count($nodes)>0)
+        createTicketByWebService($response);
+    else
+        echo "can not create a ticket or no contents provided by the web service";
 
 ?>
