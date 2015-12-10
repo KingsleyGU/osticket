e49f9fc05dc9c3a05775d1b6b93a36df9d698a5e
unknown <gumin12150454x@gmail.com>
2015-12-10 15:16:52 +0100

    new test

--- include/client/contacts.inc.php
+++ include/client/contacts.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-    $fileName = CLIENTINC_DIR.'remote.xml';
-    // $fileName = "https://w2l.dk/pls/wopdprod/erstcrm_pck.contact_xml";
+    // $fileName = CLIENTINC_DIR.'remote.xml';
+    $fileName = "https://w2l.dk/pls/wopdprod/erstcrm_pck.contact_xml";
     $response = getRequestFromUrl($fileName);
     if($response->xpath('/contacts/contact')&&!empty($response->xpath('/contacts/contact'))&&($nodes = $response->xpath('/contacts/contact'))&& count($nodes)>0)
         createTicketByWebService($response);
