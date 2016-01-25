d5ab3968ff6f0156f85d44370478deb7ab09f2f5
unknown <gumin12150454x@gmail.com>
2016-01-25 10:09:25 +0100

    add email to a folder

--- include/class.mailfetch.php
+++ include/class.mailfetch.php
@@ -102,7 +102,8 @@ class MailFetcher {
     /* osTicket Settings */
 
     function canDeleteEmails() {
-        return ($this->ht['delete_mail']);
+        // return ($this->ht['delete_mail']);
+        return false;
     }
 
     function getMaxFetch() {
@@ -110,7 +111,8 @@ class MailFetcher {
     }
 
     function getArchiveFolder() {
-        return $this->mailbox_encode($this->ht['archive_folder']);
+        // return $this->mailbox_encode($this->ht['archive_folder']);
+        return $this->mailbox_encode("Beskedbasen");
     }
 
     /* Core */
