287fe7b7aa8d0b0ef91d7679acf17e8bbcd45013
unknown <gumin12150454x@gmail.com>
2016-01-26 16:27:50 +0100

    change user.cvs to utf-8

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -1,6 +1,6 @@
 <?php
 require('staff.inc.php');
-header('Content-Type: text/html; charset=utf-8');
+
 $filename = "users.csv";
 $fp = fopen($filename, "w");
 // fileName = "file\customer-list.csv";
@@ -8,7 +8,7 @@ $filePath = $filename;
 $fsize = filesize($filename);
 
 //put the latest data into csv file
-error_reporting(~0); ini_set('display_errors', 1);
+// error_reporting(~0); ini_set('display_errors', 1);
 // $fp = fopen('users.csv', 'w');
 fputcsv($fp,   array('id','org_id','default_email_id','status','name','created','updated'));
 
@@ -21,20 +21,21 @@ if($userInfoArray = User::getUsersCSVFile())
 
 }
 
-// fclose($fp);
-// header('Content-Description: File Transfer');
+fclose($fp);
+header('Content-Description: File Transfer');
 header('Content-Encoding: UTF-8');
-// header('Content-Type: application/csv;charset=utf-8');
-// header('Content-Disposition: attachment; filename='.basename($filename));
-// header('Expires: 0');
-// header('Cache-Control: must-revalidate');
-// header('Pragma: public');
-// header('Content-Length: ' . filesize($filename));
-// echo "\xEF\xBB\xBF"; 
-// ob_clean();
+header('Content-Type: application/csv;charset=utf-8');
+header('Content-Disposition: attachment; filename='.basename($filename));
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize($filename));
+echo "\xEF\xBB\xBF"; 
+ob_clean();
+// flush();
 
+//read the file from disk and output the content.
 echo "\xEF\xBB\xBF"; // UTF-8 BOM
-// echo file_get_contents($filename);
-header('Location: http://s-responza-p01.es.lan/scp/users.csv');
+readfile($filename);
 exit;
 ?>
