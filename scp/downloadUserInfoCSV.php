2a71e3863894068721e48d405cb05baa535468a6
unknown <gumin12150454x@gmail.com>
2016-01-26 16:29:29 +0100

    change user.cvs to utf-8

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -22,20 +22,20 @@ if($userInfoArray = User::getUsersCSVFile())
 }
 
 fclose($fp);
-header('Content-Description: File Transfer');
-header('Content-Encoding: UTF-8');
-header('Content-Type: application/csv;charset=utf-8');
-header('Content-Disposition: attachment; filename='.basename($filename));
-header('Expires: 0');
-header('Cache-Control: must-revalidate');
-header('Pragma: public');
-header('Content-Length: ' . filesize($filename));
-echo "\xEF\xBB\xBF"; 
-ob_clean();
+// header('Content-Description: File Transfer');
+// header('Content-Encoding: UTF-8');
+// header('Content-Type: application/csv;charset=utf-8');
+// header('Content-Disposition: attachment; filename='.basename($filename));
+// header('Expires: 0');
+// header('Cache-Control: must-revalidate');
+// header('Pragma: public');
+// header('Content-Length: ' . filesize($filename));
+// echo "\xEF\xBB\xBF"; 
+// ob_clean();
 // flush();
 
 //read the file from disk and output the content.
 echo "\xEF\xBB\xBF"; // UTF-8 BOM
-readfile($filename);
+echo file_get_contents($filename);
 exit;
 ?>
