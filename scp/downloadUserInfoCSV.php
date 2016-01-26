3cd244ad71afedda03242b5e0dfa249d040adab0
unknown <gumin12150454x@gmail.com>
2016-01-26 15:58:36 +0100

    user csv

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -8,34 +8,32 @@ $filePath = $filename;
 $fsize = filesize($filename);
 
 //put the latest data into csv file
-// error_reporting(~0); ini_set('display_errors', 1);
+error_reporting(~0); ini_set('display_errors', 1);
 // $fp = fopen('users.csv', 'w');
 fputcsv($fp,   array('id','org_id','default_email_id','status','name','created','updated'));
 
 if($userInfoArray = User::getUsersCSVFile())
 {
-	// echo json_encode($userInfoArray);
+	echo json_encode($userInfoArray);
 	foreach ($userInfoArray as $fields) {
     fputcsv($fp, $fields);
 	}
 
 }
 
-fclose($fp);
-header('Content-Description: File Transfer');
-header('Content-Encoding: UTF-8');
-header('Content-Type: application/csv;charset=utf-8');
-header('Content-Disposition: attachment; filename='.basename($filename));
-header('Expires: 0');
-header('Cache-Control: must-revalidate');
-header('Pragma: public');
-header('Content-Length: ' . filesize($filename));
-echo "\xEF\xBB\xBF"; 
-ob_clean();
-// flush();
+// fclose($fp);
+// header('Content-Description: File Transfer');
+// header('Content-Encoding: UTF-8');
+// header('Content-Type: application/csv;charset=utf-8');
+// header('Content-Disposition: attachment; filename='.basename($filename));
+// header('Expires: 0');
+// header('Cache-Control: must-revalidate');
+// header('Pragma: public');
+// header('Content-Length: ' . filesize($filename));
+// echo "\xEF\xBB\xBF"; 
+// ob_clean();
 
-//read the file from disk and output the content.
-echo "\xEF\xBB\xBF"; // UTF-8 BOM
-readfile($filename);
-exit;
+// echo "\xEF\xBB\xBF"; // UTF-8 BOM
+// readfile($filename);
+// exit;
 ?>
