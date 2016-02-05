952213b2280d8f67968366591051811421ada1d1
unknown <gumin12150454x@gmail.com>
2016-02-05 15:45:31 +0100

    delete change admin property

--- scp/disableCollaborators.php
+++ scp/disableCollaborators.php
@@ -2,7 +2,7 @@
 
 require('staff.inc.php');
 require_once(INCLUDE_DIR.'class.ticket.php');
-if($tid=$_POST['ticketId'])
+if(($tid=$_POST['ticketId'])&&($ticket=Ticket::lookup($tid)))
 {
 	echo $tid;
 }
