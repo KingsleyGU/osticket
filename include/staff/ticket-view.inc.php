b5dab66c948660c22105ed5db299295b12138c27
unknown <gumin12150454x@gmail.com>
2016-02-05 15:27:40 +0100

    delete change admin property

--- include/staff/ticket-view.inc.php
+++ include/staff/ticket-view.inc.php
@@ -1266,20 +1266,23 @@ var accepted=aTarget.dispatchEvent (pressEvent);
 <script type="text/javascript">
 var disableCollaborators= function(ticketId)
 {
-    data = new FormData();
-    data.append( 'ticketId', ticketId);
-    $.ajax({
-         type: "POST",
-         url: '/scp/disableCollaborators.php',
-         data: data,
-         success: function(data) {
-            alert(data);
-         },
-         error: function(data) { 
-            alert(data); 
-         } 
-    });
-// alert("22222");
+    // data = new FormData();
+    // data.append('ticketId', ticketId);
+    // $.ajax({
+    //      type: "POST",
+    //      url: '/scp/disableCollaborators.php',
+    //      data: data,
+    //      success: function(data) {
+    //         alert(data);
+    //      },
+    //      error: function(data) { 
+    //         alert(data); 
+    //      } 
+    // });
+    $.post( '/scp/disableCollaborators.php', { ticketId: ticketId })
+  .done(function( data ) {
+    alert( "Data Loaded: " + data );
+  });
     return false;
 }
 </script>
