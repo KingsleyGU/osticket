6bd8c4bfc9269c52f6106406f6b19ffb7b5d2844
unknown <gumin12150454x@gmail.com>
2016-02-05 15:19:07 +0100

    delete change admin property

--- scp/tickets.php
+++ scp/tickets.php
@@ -558,3 +558,25 @@ if($thisstaff->updateAgentTicketAccess($originalStaff['assigned_only']))
 }
 $thisstaff->reload();
 require_once(STAFFINC_DIR.'footer.inc.php');
+?>
+
+<script type="text/javascript">
+var disableCollaborators= function(ticketId)
+{
+    data = new FormData();
+    data.append( 'ticketId', ticketId);
+    $.ajax({
+         type: "POST",
+         url: 'disableCollaborators.php',
+         data: data,
+         success: function(data) {
+            alert(data);
+         },
+         error: function(data) { 
+            alert(data); 
+         } 
+    });
+    return false;
+}
+</script>
+<?php
