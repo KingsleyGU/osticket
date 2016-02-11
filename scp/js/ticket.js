abeb5fef329dd8dc496b6079b00665ae4b524b76
unknown <gumin12150454x@gmail.com>
2016-02-11 21:41:49 +0100

    add CVR number to the deafult  ticket form

--- scp/js/ticket.js
+++ scp/js/ticket.js
@@ -388,8 +388,9 @@ var ticket_onload = function($) {
     //print options TODO: move to backend
     $('a#ticket-print').click(function(e) {
         e.preventDefault();
-        $('#overlay').show();
-        $('.dialog#print-options').show();
+        // $('#overlay').show();
+        // $('.dialog#print-options').show();
+        $("form#print-form").submit();
         return false;
     });
 
