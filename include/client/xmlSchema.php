beb6e769c4ca79d143a51e5826f807b798862953
unknown <gumin12150454x@gmail.com>
2016-02-15 10:06:08 +0100

    grab the subject2 as the title

--- include/client/xmlSchema.php
+++ include/client/xmlSchema.php
@@ -40,8 +40,10 @@ require_once(INCLUDE_DIR.'class.dynamic_forms.php');
                     $data =array();
                     // $data['recipients'] = array();
                     $data['subject'] = removeLineBreaker($nodes[$i]->title);
-                    if(empty( $data['subject']))
-                         $data['subject'] = "no title";
+                    if(empty( $data['subject'])&&!empty(removeLineBreaker($nodes[$i]->crmsubject2_text)))
+                        $data['subject'] = removeLineBreaker($nodes[$i]->crmsubject2_text);
+                    else
+                        $data['subject'] = "no title";
                     $data['header'] = "";
                     // $data['mid'] = 1;
                     $data['source'] = "Web";
