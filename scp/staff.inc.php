78649d6c881e422657fbbbbdaeeaac136354ca13
unknown <gumin12150454x@gmail.com>
2016-02-17 20:05:44 +0100

    make the search function not working

--- scp/staff.inc.php
+++ scp/staff.inc.php
@@ -112,11 +112,11 @@ function changeStaffToOrigin()
 function backToSearchPage()
 {
     if(isset($_SESSION['previousPageUrl'])&&!empty($_SESSION['previousPageUrl']))
-    // echo "<script>                
-    //     window.history.go(-2);
-    //    </script>";
+      echo "<script>                
+        window.history.go(-2);
+       </script>";
 // if(isset($previousURL)&&!empty($previousURL))
-     header("Location: " . $_SESSION['previousPageUrl']);
+     // header("Location: " . $_SESSION['previousPageUrl']);
 }
 // Bootstrap gettext translations as early as possible, but after attempting
 // to sign on the agent
