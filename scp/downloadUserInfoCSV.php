0ddb625e18a400dfdb03b4a16e9a6d19247473e0
unknown <gumin12150454x@gmail.com>
2016-02-22 10:55:35 +0100

    update download staff csv file

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -10,7 +10,8 @@ $fsize = filesize($filename);
 //put the latest data into csv file
 error_reporting(~0); ini_set('display_errors', 1);
 // $fp = fopen('users.csv', 'w');
-fputcsv($fp, array('username','firstname','lastname','isadmin','onvacation','created','lastlogin'));
+// username,firstname,lastname,isadmin,onvacation,lastlogin
+fputcsv($fp,   array('username','firstname','lastname','isadmin','onvacation','created','lastlogin'));
 
 if($userInfoArray = Staff::getStaffCSVFile())
 {
@@ -22,27 +23,12 @@ if($userInfoArray = Staff::getStaffCSVFile())
 
 }
 
-
 fclose($fp);
-// header('Content-Description: File Transfer');
-// header('Content-Encoding: UTF-8');
-
 
 header('Content-Type: application/csv;charset=utf-8');
 header('Content-Disposition: attachment; filename='.basename($filename));
-
-
-// header('Expires: 0');
-// header('Cache-Control: must-revalidate');
-// header('Pragma: public');
-// header('Content-Length: ' . filesize($filename));
-// echo "\xEF\xBB\xBF"; 
-// ob_clean();
-// flush();
-
-// //read the file from disk and output the content.
-echo "\xEF\xBB\xBF"; // UTF-8 BOM
-readfile($filename);
+echo "\xEF\xBB\xBF"; 
+ readfile($filename);
 
 // exit;
 ?>
