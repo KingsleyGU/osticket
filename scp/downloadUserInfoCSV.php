63471245b0471f5c60bd1da8daea8a6447a8b22f
unknown <gumin12150454x@gmail.com>
2016-02-22 11:17:53 +0100

    update download staff csv file

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -12,13 +12,14 @@ error_reporting(~0); ini_set('display_errors', 1);
 // $fp = fopen('users.csv', 'w');
 // header('Content-Type: application/csv;charset=utf-8');
 // header('Content-Disposition: attachment; filename='.basename($filename));
+// fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
 fputcsv($fp,   array('id','org_id','default_email_id','status','name','created','updated'));
 
 if($userInfoArray = User::getUsersCSVFile())
 {
 	echo json_encode($userInfoArray);
 	foreach ($userInfoArray as $fields) {
-	fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
+	// fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
     fputcsv($fp, $fields);
 	}
 
