3953f6a7d624a07c2f155801722e3f2a9241de98
KingsleyGu <gumin12150454x@gmail.com>
2016-03-17 12:50:40 +0100

    make the changes

--- include/client/contacts.inc.php
+++ include/client/contacts.inc.php
@@ -5,13 +5,13 @@
     $response = getRequestFromUrl($fileName);
     if($response->xpath('/contacts/contact')&&!empty($response->xpath('/contacts/contact'))&&($nodes = $response->xpath('/contacts/contact'))&& count($nodes)>0)
         createTicketByWebService($response);
-    else
-    {
-       echo "can not create a ticket or no contents provided by the web service <br/>";
-       $fileName = CLIENTINC_DIR.'remote.xml';
-       $response = getRequestFromUrl($fileName);
-       createTicketByWebService($response);
-       echo "a test case has been generated <br/>";
-	   }
+    // else
+    // {
+    //    echo "can not create a ticket or no contents provided by the web service <br/>";
+    //    $fileName = CLIENTINC_DIR.'remote.xml';
+    //    $response = getRequestFromUrl($fileName);
+    //    createTicketByWebService($response);
+    //    echo "a test case has been generated <br/>";
+	   // }
 
 ?>
