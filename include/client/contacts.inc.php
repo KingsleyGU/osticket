24e819026d22a14bf85dd42cd64e957d1b5d8a1d
KingsleyGu <gumin12150454x@gmail.com>
2016-03-22 11:45:16 +0100

    make a global variables

--- include/client/contacts.inc.php
+++ include/client/contacts.inc.php
@@ -6,10 +6,10 @@
     $response = getRequestFromUrl($fileName);
     if($response->xpath('/contacts/contact')&&!empty($response->xpath('/contacts/contact'))&&($nodes = $response->xpath('/contacts/contact'))&& count($nodes)>0)
     {  
-    	echo $logFilePath;
-    	error_log("it is", 3, $logFilePath);
         createTicketByWebService($response);
     }
+	echo $logFilePath;
+	error_log("it is", 3, $logFilePath);
 //     else
 //     {
 //        echo "can not create a ticket or no contents provided by the web service <br/>";
