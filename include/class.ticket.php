580813dda2965c774cdb68b0720d9a75a71ff439
KingsleyGu <gumin12150454x@gmail.com>
2016-03-23 20:35:16 +0100

    make changes

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -2310,7 +2310,7 @@ class Ticket {
                 file_put_contents(INCLUDE_DIR.'pdfConverter/'."test.docx", $fileData);
                 $cmd = "chmod -R 777 ".INCLUDE_DIR.'pdfConverter/'."test.docx";
                 shell_exec($cmd);
-                $cmd = 'export HOME=/tmp && /usr/bin/libreoffice5.0 --headless --invisible --norestore --convert-to pdf '.INCLUDE_DIR.'pdfConverter/'."test.docx";
+                $cmd = 'export HOME=/tmp && /usr/bin/libreoffice5.0 --headless --convert-to pdf --outdir '.INCLUDE_DIR.'pdfConverter/'." ".INCLUDE_DIR.'pdfConverter/'."test.docx";
                 $this->logErrors("2222222 ".$cmd);
                 system($cmd);
                 
