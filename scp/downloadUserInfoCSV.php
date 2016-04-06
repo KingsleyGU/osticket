90943128147dc6996317c22944396e21c52aee5f
KingsleyGu <gumin12150454x@gmail.com>
2016-04-06 14:05:54 +0200

    make the download users files working

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -8,13 +8,13 @@ $filePath = $filename;
 $fsize = filesize($filename);
 
 //put the latest data into csv file
-// error_reporting(~0); ini_set('display_errors', 1);
-header('Content-Transfer-Encoding: binary');  // For Gecko browsers mainly
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filePath)) . ' GMT');
-header('Accept-Ranges: bytes');  // Allow support for download resume
-header('Content-Length: ' . filesize($filePath));  // File size
+error_reporting(~0); ini_set('display_errors', 1);
+// header('Content-Transfer-Encoding: binary');  // For Gecko browsers mainly
+// header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filePath)) . ' GMT');
+// header('Accept-Ranges: bytes');  // Allow support for download resume
+// header('Content-Length: ' . filesize($filePath));  // File size
 // header('Content-Type: application/csv;charset=utf-8');
-header('Content-Disposition: attachment; filename='.basename($filename));
+// header('Content-Disposition: attachment; filename='.basename($filename));
 fputcsv($fp,   array('username','firstname','lastname','isadmin','onvacation','created','lastlogin'));
 
 if($userInfoArray = Staff::getStaffCSVFile())
