07689088410378b8104eb7c82585fa1f63aab149
KingsleyGu <gumin12150454x@gmail.com>
2016-04-06 16:05:50 +0200

    make changes just for test

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -12,13 +12,13 @@ $fsize = filesize($filename);
 //put the latest data into csv file
 // error_reporting(~0); ini_set('display_errors', 1);
 // ini_set('auto_detect_line_endings',TRUE);
-// header('Content-Transfer-Encoding: binary');  // For Gecko browsers mainly
-// header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filePath)) . ' GMT');
-// header('Accept-Ranges: bytes');  // Allow support for download resume
-// header('Content-Length: ' . filesize($filePath));  // File size
-// header('Content-Type: application/csv;charset=utf-8');
-// header('Content-Disposition: attachment; filename='.basename($filename));
-// fputcsv($fp,   array('username','firstname','lastname','isadmin','onvacation','created','lastlogin','teams'));
+header('Content-Transfer-Encoding: binary');  // For Gecko browsers mainly
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filePath)) . ' GMT');
+header('Accept-Ranges: bytes');  // Allow support for download resume
+header('Content-Length: ' . filesize($filePath));  // File size
+header('Content-Type: application/csv;charset=utf-8');
+header('Content-Disposition: attachment; filename='.basename($filename));
+fputcsv($fp,   array('username','firstname','lastname','isadmin','onvacation','created','lastlogin','teams'));
 
 if($userInfoArray = Staff::getStaffCSVFile())
 {
@@ -28,7 +28,7 @@ if($userInfoArray = Staff::getStaffCSVFile())
 	fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
 	// fputcsv($fp,$fields);
 	$teams = Staff::getStaffTeams($fields['staff_id']);
-	echo $teams;
+	// echo $teams;
     fputcsv($fp, array($fields['username'],$fields['firstname'],$fields['lastname'],$fields['isadmin'],$fields['onvacation'],$fields['created'],$fields['lastlogin'],$teams));
 	}
 
@@ -36,11 +36,11 @@ if($userInfoArray = Staff::getStaffCSVFile())
 
 fclose($fp);
 
-
-// echo "\xEF\xBB\xBF"; 
-// header('Content-Type: application/csv;charset=utf-8');
-// header('Content-Disposition: attachment; filename='.basename($filename));
-// readfile($filename);
+ini_set('auto_detect_line_endings',TRUE);
+echo "\xEF\xBB\xBF"; 
+header('Content-Type: application/csv;charset=utf-8');
+header('Content-Disposition: attachment; filename='.basename($filename));
+readfile($filename);
 
 exit;
 ?>
