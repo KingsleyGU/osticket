80d97b26036520bc00d210ceac206633ec31b168
KingsleyGu <gumin12150454x@gmail.com>
2016-04-06 15:45:42 +0200

    make the download users files working

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -12,21 +12,21 @@ $fsize = filesize($filename);
 //put the latest data into csv file
 // error_reporting(~0); ini_set('display_errors', 1);
 // ini_set('auto_detect_line_endings',TRUE);
-// header('Content-Transfer-Encoding: binary');  // For Gecko browsers mainly
-// header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filePath)) . ' GMT');
-// header('Accept-Ranges: bytes');  // Allow support for download resume
-// header('Content-Length: ' . filesize($filePath));  // File size
-// header('Content-Type: application/csv;charset=utf-8');
-// header('Content-Disposition: attachment; filename='.basename($filename));
-// fputcsv($fp,   array('username','firstname','lastname','isadmin','onvacation','created','lastlogin','teams'));
+header('Content-Transfer-Encoding: binary');  // For Gecko browsers mainly
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filePath)) . ' GMT');
+header('Accept-Ranges: bytes');  // Allow support for download resume
+header('Content-Length: ' . filesize($filePath));  // File size
+header('Content-Type: application/csv;charset=utf-8');
+header('Content-Disposition: attachment; filename='.basename($filename));
+fputcsv($fp,   array('username','firstname','lastname','isadmin','onvacation','created','lastlogin','teams'));
 
 if($userInfoArray = Staff::getStaffCSVFile())
 {
 	// echo json_encode($userInfoArray);
 	foreach ($userInfoArray as $fields) {
-		echo json_encode(array($fields['username'],$fields['firstname'],$fields['lastname'],$fields['isadmin'],$fields['onvacation'],$fields['created'],$fields['lastlogin'],Staff::getStaffTeams($fields['staff_id'])));
+		// echo json_encode(array($fields['username'],$fields['firstname'],$fields['lastname'],$fields['isadmin'],$fields['onvacation'],$fields['created'],$fields['lastlogin'],Staff::getStaffTeams($fields['staff_id'])));
 	fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
-    // fputcsv($fp, array($fields['username'],$fields['firstname'],$fields['lastname'],$fields['isadmin'],$fields['onvacation'],$fields['created'],$fields['lastlogin'],Staff::getStaffTeams($fields['staff_id'])));
+    fputcsv($fp, array($fields['username'],$fields['firstname'],$fields['lastname'],$fields['isadmin'],$fields['onvacation'],$fields['created'],$fields['lastlogin'],Staff::getStaffTeams($fields['staff_id'])));
 	}
 
 }
@@ -34,10 +34,10 @@ if($userInfoArray = Staff::getStaffCSVFile())
 fclose($fp);
 
 
-// echo "\xEF\xBB\xBF"; 
-// header('Content-Type: application/csv;charset=utf-8');
-// header('Content-Disposition: attachment; filename='.basename($filename));
-// readfile($filename);
+echo "\xEF\xBB\xBF"; 
+header('Content-Type: application/csv;charset=utf-8');
+header('Content-Disposition: attachment; filename='.basename($filename));
+readfile($filename);
 
 exit;
 ?>
