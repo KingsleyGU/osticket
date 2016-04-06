49e89a9e63c80b9202b69be7386d8d64f57739e1
KingsleyGu <gumin12150454x@gmail.com>
2016-04-06 14:08:06 +0200

    make the download users files working

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -8,7 +8,7 @@ $filePath = $filename;
 $fsize = filesize($filename);
 
 //put the latest data into csv file
-error_reporting(~0); ini_set('display_errors', 1);
+// error_reporting(~0); ini_set('display_errors', 1);
 header('Content-Transfer-Encoding: binary');  // For Gecko browsers mainly
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filePath)) . ' GMT');
 header('Accept-Ranges: bytes');  // Allow support for download resume
