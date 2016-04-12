9f5fca62bc4e783f3a813cd762b3d72e33fd8bdb
KingsleyGu <gumin12150454x@gmail.com>
2016-04-12 12:40:23 +0200

    make the changes back

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -76,7 +76,7 @@ fclose($fp);
 
 // ini_set('auto_detect_line_endings',TRUE);
 echo "\xEF\xBB\xBF"; 
-header('Content-Type: application/csv;charset=utf-8');
+header('Content-Type: application/csv;');
 header('Content-Disposition: attachment; filename='.basename($filename));
 readfile($filename);
 
