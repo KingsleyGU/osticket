0087937ec0e8e7998092b18982cedad5d04fd398
KingsleyGu <gumin12150454x@gmail.com>
2016-04-12 13:29:49 +0200

    make the changes back

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -10,15 +10,15 @@ $filePath = $filename;
 $fsize = filesize($filename);
 
 //put the latest data into csv file
-error_reporting(~0); ini_set('display_errors', 1);
+// error_reporting(~0); ini_set('display_errors', 1);
 // ini_set('auto_detect_line_endings',TRUE);
-// ini_set('auto_detect_line_endings',TRUE);
-// header('Content-Transfer-Encoding: binary');  // For Gecko browsers mainly
-// header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filePath)) . ' GMT');
-// header('Accept-Ranges: bytes');  // Allow support for download resume
-// header('Content-Length: ' . filesize($filePath));  // File size
-// header('Content-Type: application/csv;charset=utf-8');
-// header('Content-Disposition: attachment; filename='.basename($filename));
+ini_set('auto_detect_line_endings',TRUE);
+header('Content-Transfer-Encoding: binary');  // For Gecko browsers mainly
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($filePath)) . ' GMT');
+header('Accept-Ranges: bytes');  // Allow support for download resume
+header('Content-Length: ' . filesize($filePath));  // File size
+header('Content-Type: application/csv;charset=utf-8');
+header('Content-Disposition: attachment; filename='.basename($filename));
 $titleArray =  array('username','firstname','lastname','isadmin','onvacation','created','lastlogin');
 $teamsArray = Team::getActiveTeams();
 // logErrors(json_encode($teamsArray));
@@ -47,7 +47,7 @@ if($userInfoArray = Staff::getStaffCSVFile())
 	// echo json_encode($userInfoArray);
 	foreach ($userInfoArray as $fields) {
 			// echo json_encode(array($fields['username'],$fields['firstname'],$fields['lastname'],$fields['isadmin'],$fields['onvacation'],$fields['created'],$fields['lastlogin'],Staff::getStaffTeams($fields['staff_id'])));
-		// fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
+		fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
 		// fputcsv($fp,$fields);
 		$teams = Staff::getStaffTeams($fields['staff_id']);
 		// logErrors(json_encode(Team::getActiveTeams()));
@@ -58,7 +58,7 @@ if($userInfoArray = Staff::getStaffCSVFile())
 			// fputcsv($fp,$resultArray);
 			logErrors(json_encode($teamsArray));
 			$newTeamsArray = Team::getActiveTeams();
-			foreach ($newTeamsArray as $value) {
+			// foreach ($newTeamsArray as $value) {
 				// logErrors("team id: ".$key);
 				// if($team = Team::lookup(intval($key)))
 				// {
@@ -66,7 +66,7 @@ if($userInfoArray = Staff::getStaffCSVFile())
 					// array_push($resultArray,booleanToString($team->hasMember(Staff::lookup(intval($fields['staff_id'])))));	
 				array_push($resultArray,1);
 				// }
-			}
+			// }
 			// logErrors(json_encode($resultArray));
 			fputcsv($fp,$resultArray);
 	    // fputcsv($fp, array_merge(array($fields['username'],$fields['firstname'],$fields['lastname'],$fields['isadmin'],$fields['onvacation'],$fields['created'],$fields['lastlogin']),null));
@@ -82,16 +82,16 @@ fclose($fp);
 
 // ini_set('auto_detect_line_endings',TRUE);
 // echo "\xEF\xBB\xBF"; 
-// header("Cache-Control: no-store, no-cache, must-revalidate"); 
-// header("Cache-Control: post-check=0, pre-check=0", false); 
-// header("Cache-Control: private",false); 
-// header("Pragma: no-cache");
-// header('Content-Length: ' . filesize($filePath));
-// header('Content-Type: application/csv;charset=utf-8');
-// header("Content-Transfer-Encoding: binary");
-// header('Content-Disposition: attachment; filename='.basename($filename));
-// // echo file_get_contents($filename);
-// echo  readfile($filename);
-// ini_set('auto_detect_line_endings',FALSE);
+header("Cache-Control: no-store, no-cache, must-revalidate"); 
+header("Cache-Control: post-check=0, pre-check=0", false); 
+header("Cache-Control: private",false); 
+header("Pragma: no-cache");
+header('Content-Length: ' . filesize($filePath));
+header('Content-Type: application/csv;charset=utf-8');
+header("Content-Transfer-Encoding: binary");
+header('Content-Disposition: attachment; filename='.basename($filename));
+// echo file_get_contents($filename);
+echo  readfile($filename);
+ini_set('auto_detect_line_endings',FALSE);
 exit;
 ?>
