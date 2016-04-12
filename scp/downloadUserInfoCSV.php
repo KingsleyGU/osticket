5804f68437bf09d4e7865cde1738237216099e8d
KingsleyGu <gumin12150454x@gmail.com>
2016-04-12 11:25:33 +0200

    test for the title

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -62,8 +62,9 @@ if($userInfoArray = Staff::getStaffCSVFile())
 				}
 			}
 			// logErrors(json_encode($resultArray));
-			
-	    fputcsv($fp, array_merge(array($fields['username'],$fields['firstname'],$fields['lastname'],$fields['isadmin'],$fields['onvacation'],$fields['created'],$fields['lastlogin']),null));
+
+			fputcsv($fp,$resultArray);
+	    // fputcsv($fp, array_merge(array($fields['username'],$fields['firstname'],$fields['lastname'],$fields['isadmin'],$fields['onvacation'],$fields['created'],$fields['lastlogin']),null));
 		
 		} catch (Exception $e) {
 			logErrors('Caught exception: ',  $e->getMessage(), "\n");
