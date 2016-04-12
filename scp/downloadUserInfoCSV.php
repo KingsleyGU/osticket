f5048d65eb271961b54899b87d4021faad540fa8
KingsleyGu <gumin12150454x@gmail.com>
2016-04-12 13:01:59 +0200

    make the changes back

--- scp/downloadUserInfoCSV.php
+++ scp/downloadUserInfoCSV.php
@@ -56,13 +56,13 @@ if($userInfoArray = Staff::getStaffCSVFile())
 			$resultArray = array($fields['username'],$fields['firstname'],$fields['lastname'],$fields['isadmin'],$fields['onvacation'],$fields['created'],$fields['lastlogin']);
 			// fputcsv($fp,$resultArray);
 			foreach ($teamsArray as $key => $value) {
-				logErrors("team id: ".$key);
-				if($team = Team::lookup(intval($key)))
-				{
+				// logErrors("team id: ".$key);
+				// if($team = Team::lookup(intval($key)))
+				// {
 					// logErrors("staff id: ".$fields['staff_id']);
 					// array_push($resultArray,booleanToString($team->hasMember(Staff::lookup(intval($fields['staff_id'])))));	
-					array_push($resultArray,1);
-				}
+					array_push($resultArray,$key);
+				// }
 			}
 			logErrors(json_encode($resultArray));
 			fputcsv($fp,$resultArray);
