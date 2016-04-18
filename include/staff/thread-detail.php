6693a0b082cc23bc3cabc678c48e7bfc488f2d46
KingsleyGu <gumin12150454x@gmail.com>
2016-04-18 10:22:50 +0200

    make the num hide

--- include/staff/thread-detail.php
+++ include/staff/thread-detail.php
@@ -1,1 +1,90 @@
-111111111111111111111
+<?php
+if($_REQUEST['id']) {
+    if(!($ticket=Ticket::lookup($_REQUEST['id'])))
+         $errors['err']=sprintf(__('%s: Unknown or invalid ID.'), __('ticket'));
+ }
+
+?>
+<?php
+$tcount = $ticket->getThreadCount();
+// $tcount+= $ticket->getNumNotes();
+?>
+<ul id="threads">
+    <li><a class="active" id="toggle_ticket_thread" href="#"><?php echo sprintf(__('Ticket Thread (%d)'), $tcount); ?></a></li>
+</ul>
+<div id="ticket_thread">
+    <?php
+    $threadTypes=array('M'=>'message','R'=>'response', 'N'=>'note');
+    /* -------- Messages & Responses & Notes (if inline)-------------*/
+    $types = array('M', 'R', 'N');
+    if(($thread=$ticket->getThreadEntries($types))) {
+       foreach($thread as $entry) { ?>
+        <table class="thread-entry <?php echo $threadTypes[$entry['thread_type']]; ?>" cellspacing="0" cellpadding="1" width="940" border="0">
+            <tr>
+                <th colspan="4" width="100%">
+                <div>
+                    <span class="pull-left">
+                    <span style="display:inline-block"><?php
+                        echo Format::db_datetime($entry['created']);?></span>
+                    <span style="display:inline-block;padding:0 1em" class="faded title"><?php
+                        echo Format::truncate($entry['title'], 100); ?></span>
+                    </span>  
+
+                    <?php 
+                        if($entry['thread_type'] != 'N')
+                        {
+                    ?>
+                    <div class="pull-right forward_thread_block" style="display:none">
+                    <input type="checkbox"  name="forward_thread_choice" class="forward_thread_choice" value="<?php echo $entry['id']; ?>" onclick='checkForwardThreadList();'> Forward
+                    </div>
+                    <?php 
+                        }
+                    ?>   
+
+                    <span class="pull-right" style="white-space:no-wrap;display:inline-block" >
+                        <span style="vertical-align:middle;" class="textra"></span>
+                        <span style="vertical-align:middle;"
+                            class="tmeta faded title"><?php
+                            echo Format::htmlchars($entry['name'] ?: $entry['poster']); ?></span>
+                    </span>                    
+                </div>
+                </th>
+            </tr>
+            <tr><td colspan="4" class="thread-body" id="thread-id-<?php
+                echo $entry['id']; ?>"><div><?php
+                echo $entry['body']->toHtml(); ?></div></td></tr>
+            <?php
+            if($entry['attachments']
+                    && ($tentry = $ticket->getThreadEntry($entry['id']))
+                    && ($urls = $tentry->getAttachmentUrls())
+                    && ($links = $tentry->getAttachmentsLinks())) {?>
+            <tr>
+                <td class="info" colspan="4"><?php echo $tentry->getAttachmentsLinks(); ?></td>
+            </tr> <?php
+            }
+            if ($urls) { ?>
+                <script type="text/javascript">
+                    $('#thread-id-<?php echo $entry['id']; ?>')
+                        .data('urls', <?php
+                            echo JsonDataEncoder::encode($urls); ?>)
+                        .data('id', <?php echo $entry['id']; ?>);
+                </script>
+<?php
+            } ?>
+        </table>
+        <?php
+        if($entry['thread_type']=='M')
+            $msgId=$entry['id'];
+       }
+    } else {
+        echo '<p>'.__('Error fetching ticket thread - get technical help.').'</p>';
+    }?>
+</div>
+<div class="clear" style="padding-bottom:10px;"></div>
+<?php if($errors['err']) { ?>
+    <div id="msg_error"><?php echo $errors['err']; ?></div>
+<?php }elseif($msg) { ?>
+    <div id="msg_notice"><?php echo $msg; ?></div>
+<?php }elseif($warn) { ?>
+    <div id="msg_warning"><?php echo $warn; ?></div>
+<?php } ?>
