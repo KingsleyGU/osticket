14f95df715137d08744304b0437c4cc76f7b4442
KingsleyGu <gumin12150454x@gmail.com>
2016-05-04 10:45:13 +0200

    make changes

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -2093,7 +2093,9 @@ class Ticket {
             //         return null;
             if(!($response = $this->getThread()->addResponse($vars, $errors)))
             {  
-                $responseBody = $responseBody ."<br>--------------Answer from ERST--------------<br>";  
+                $response->setBody(ThreadBody::fromFormattedText($responseBody, $response->ht['format']));
+                $response->reload();
+                $responseBody = $responseBody ."<br>--------------Reply from ERST--------------<br>";  
                 $responseBody.$response->ht['body'];
             }
             foreach ($threadIdList as $threadId) 
@@ -2103,9 +2105,9 @@ class Ticket {
                     $response = ThreadEntry::lookup(intval($threadId));
                     // return null;
                     if($response->getType()=='M')
-                        $responseBody = $responseBody ."<br>--------------Question--------------<br>";
+                        $responseBody = $responseBody ."<br>--------------Message from Client--------------<br>";
                     else
-                        $responseBody = $responseBody ."<br>--------------Answer from ERST--------------<br>";                    
+                        $responseBody = $responseBody ."<br>--------------Reply from ERST--------------<br>";                    
                     $responseBody = $responseBody.$response->ht['body'];
                     $finalThreadBody = $response->ht['body'];
                     $attachments = array_merge($attachments, $response->getAttachments());
