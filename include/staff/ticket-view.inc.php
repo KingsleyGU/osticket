cdd2c87ef5def7486c2de0b877fd85be34d6d90f
KingsleyGu <gumin12150454x@gmail.com>
2016-05-09 11:49:11 +0200

    make the reply also can select multiple thread

--- include/staff/ticket-view.inc.php
+++ include/staff/ticket-view.inc.php
@@ -978,7 +978,7 @@ $tcount = $ticket->getThreadCount();
                     ?>
 
                     <div class="pull-right forward_thread_block" >
-                    <input type="checkbox"  name="forward_thread_choice" class="forward_thread_choice" value="<?php echo $entry['id']; ?>" onclick='checkForwardThreadList();'> Forward
+                    <input type="checkbox"  name="forward_thread_choice" class="forward_thread_choice" value="<?php echo $entry['id']; ?>" onclick='checkForwardThreadList();' checked> Forward
                     </div>
                     <?php 
                         }
