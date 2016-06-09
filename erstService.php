9f9bc8c7716f270e4748d90458b2f9a10b83b7e4
KingsleyGu <gumin12150454x@gmail.com>
2016-06-09 17:02:54 +0200

    store the files in the server

--- erstService.php
+++ erstService.php
@@ -1,6 +1,6 @@
 <?php 
 $logFilePath = "/var/log/osticket_erst_service_log";
-$CRMFilePath = "/var/www/html/erstFile/";
+$CRMFilePath = "/var/log/erstFile/";
 require('client.inc.php');
 require(CLIENTINC_DIR.'xmlSchema.php');
 require(CLIENTINC_DIR.'contacts.inc.php');
