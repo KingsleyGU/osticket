f2116cba73f6ddd1ce5d670d25c9fea31a9d13f9
KingsleyGu <gumin12150454x@gmail.com>
2016-06-09 13:29:14 +0200

    make changes to the thread

--- include/staff/thread-detail.inc.php
+++ include/staff/thread-detail.inc.php
@@ -41,7 +41,7 @@ $tcount = $ticket->getThreadCount();
                         }
                         $click_href = "tickets.php?edit_thread=1&ticket_id=".$_REQUEST['id']."&thread_id=".$entry['id'];
                     ?>   
-                    <a onclick="window.open("<?php echo $click_href; ?>;return false;")" target="_blank" href="javascript:void(0);" class="pull-right">Edit thread</a>
+                    <a onclick='window.open("<?php echo $click_href; ?>;return false;")' target="_blank" href="javascript:void(0);" class="pull-right">Edit thread</a>
 
                     <span class="pull-right" style="white-space:no-wrap;display:inline-block" >
                         <span style="vertical-align:middle;" class="textra"></span>
