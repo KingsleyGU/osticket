d1fd53770da7b742695430597f7a609ddf51e646
KingsleyGu <gumin12150454x@gmail.com>
2016-06-13 13:26:24 +0200

    make the upload file to thread

---
+++ scp/customizedAPI/uploadFilesToThread.php
@@ -0,0 +1,19 @@
+<?php
+require('staff.inc.php');
+require_once(INCLUDE_DIR.'class.attachment.php');
+require_once(INCLUDE_DIR.'class.thread.php');
+
+
+if(!isset($_REQUEST['thread_id']))
+{
+	echo ("Thread ID not provided");
+}
+elseif(is_numeric($_REQUEST['thread_id'])&&$thread = ThreadEntry::lookup($_REQUEST['thread_id']))
+{
+	$thread->saveAttachments($_REQUEST['attach:response[]']);
+	header("Location: /scp/tickets.php?id=".$_REQUEST['ticket_id']);
+}
+
+
+
+?>
