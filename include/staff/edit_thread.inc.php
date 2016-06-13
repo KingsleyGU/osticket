90773ae09219e1a036fd9acc9d54a747521b6b78
KingsleyGu <gumin12150454x@gmail.com>
2016-06-13 11:38:54 +0200

    make it possible to add files to a thread

--- include/staff/edit_thread.inc.php
+++ include/staff/edit_thread.inc.php
@@ -1,5 +1,9 @@
-
-                <div id="reply_form_attachments" class="attachments">
-<?php
-print $response_form->getField('attachments')->render();
-?>
+<form method="post" action="/scp/uploadFilesToThread">
+    <input type="hidden" name="ticket_id" value="<?php echo $_REQUEST['ticket_id'];?>">
+    <input type="hidden" name="thread_id" value="<?php echo $_REQUEST['thread_id'];?>">
+    <div id="reply_form_attachments" class="attachments">
+    <?php
+    print $response_form->getField('attachments')->render();
+    ?>
+    <input type='submit' value="Save">
+</form>
