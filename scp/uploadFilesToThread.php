c780b5a2fd7cbbc9c54c990da8aa1146ed50283b
KingsleyGu <gumin12150454x@gmail.com>
2016-06-13 14:07:39 +0200

    make the upload file to thread

--- scp/uploadFilesToThread.php
+++ scp/uploadFilesToThread.php
@@ -5,15 +5,15 @@ require_once(INCLUDE_DIR.'class.thread.php');
 
 error_reporting(~0); ini_set('display_errors', 1);
 echo "1111";
-// if(!isset($_REQUEST['thread_id']))
-// {
-// 	echo ("Thread ID not provided");
-// }
-// elseif(is_numeric($_REQUEST['thread_id'])&&$thread = ThreadEntry::lookup($_REQUEST['thread_id']))
-// {
-// 	$thread->saveAttachments($_REQUEST['attach:response[]']);
-// 	header("Location: /osticket/scp/tickets.php?id=".$_REQUEST['ticket_id']);
-// }
+if(!isset($_REQUEST['thread_id']))
+{
+	echo ("Thread ID not provided");
+}
+elseif(is_numeric($_REQUEST['thread_id'])&&($thread = ThreadEntry::lookup($_REQUEST['thread_id'])))
+{
+	$thread->saveAttachments($_REQUEST['attach:response[]']);
+	header("Location: /osticket/scp/tickets.php?id=".$_REQUEST['ticket_id']);
+}
 
 
 ?>
