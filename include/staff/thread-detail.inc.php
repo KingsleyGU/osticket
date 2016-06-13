a4e51d74200c017d380be96040d664c6dee34b41
KingsleyGu <gumin12150454x@gmail.com>
2016-06-13 15:33:04 +0200

    make the upload file to thread

--- include/staff/thread-detail.inc.php
+++ include/staff/thread-detail.inc.php
@@ -41,7 +41,7 @@ $tcount = $ticket->getThreadCount();
                         }
                         $click_href = "tickets.php?edit_thread=1&ticket_id=".$_REQUEST['id']."&thread_id=".$entry['id'];
                     ?>   
-                    <a onclick='window.open("<?php echo $click_href; ?>")' target="_blank" href="javascript:void(0);" class="pull-right">&nbsp; &nbsp;&nbsp;&nbsp;Add file </a>
+                    <a onclick='window.open("<?php echo $click_href; ?>")' target="_blank" href="javascript:void(0);" class="pull-right" style="margin-left:15px;">Add file </a>
 
                     <span class="pull-right" style="white-space:no-wrap;display:inline-block" >
                         <span style="vertical-align:middle;" class="textra"></span>
