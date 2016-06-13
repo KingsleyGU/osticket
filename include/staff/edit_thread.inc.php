078f7248f8d8ee0163c420f80661d2b8799fbe19
KingsleyGu <gumin12150454x@gmail.com>
2016-06-13 14:12:30 +0200

    make the upload file to thread

--- include/staff/edit_thread.inc.php
+++ include/staff/edit_thread.inc.php
@@ -1,4 +1,5 @@
 <form method="POST" action="/osticket/scp/uploadFilesToThread.php">
+    <?php csrf_token(); ?>
     <input type="hidden" name="ticket_id" value="<?php echo $_REQUEST['ticket_id'];?>">
     <input type="hidden" name="thread_id" value="<?php echo $_REQUEST['thread_id'];?>">
     <div id="reply_form_attachments" class="attachments">
