02996c416959709382f8c1fff3154d29c39f8039
KingsleyGu <gumin12150454x@gmail.com>
2016-06-30 12:56:28 +0200

    test for the lock issue

--- scp/testForLock.php
+++ scp/testForLock.php
@@ -3,7 +3,8 @@ require('staff.inc.php');
 require_once(INCLUDE_DIR.'class.spent_time.php');
 
 
-$currentTime = new DateTime(date('Y-m-d H:i:s'));
+$now = new DateTime();
+$currentTime = $now->format('Y-m-d H:i:s');
 Spent_time::create(1,1,$currentTime);
 
 
