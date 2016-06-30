1a08b4ab0eb082705d376997c04f2e454b3ad0b1
KingsleyGu <gumin12150454x@gmail.com>
2016-06-30 13:01:30 +0200

    test for the lock issue

--- scp/testForLock.php
+++ scp/testForLock.php
@@ -5,6 +5,7 @@ require_once(INCLUDE_DIR.'class.spent_time.php');
 error_reporting(~0); ini_set('display_errors', 1);
 $currentTime = new DateTime('0000-00-00 00:00:00', new DateTimeZone('Europe/Paris'));
 // $currentTime = $now->format('Y-m-d H:i:s');
+echo $currentTime;
 Spent_time::create(1,1,$currentTime);
 
 
