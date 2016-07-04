f65c62f703ddbabb0c58f0c984dcff474f16801e
KingsleyGu <gumin12150454x@gmail.com>
2016-07-04 09:50:14 +0200

    change the edit thread

--- include/staff/edit_thread.inc.php
+++ include/staff/edit_thread.inc.php
@@ -14,7 +14,9 @@
     <?php csrf_token(); ?>
     <input type="hidden" name="ticket_id" value="<?php echo $_REQUEST['ticket_id'];?>">
     <input type="hidden" name="thread_id" value="<?php echo $_REQUEST['thread_id'];?>">
-    <textarea><?php echo $threadContent; ?></textarea>
+
+        <?php echo $threadContent; ?>
+
     <div id="reply_form_attachments" class="attachments">
     <?php
     print $response_form->getField('attachments')->render();
