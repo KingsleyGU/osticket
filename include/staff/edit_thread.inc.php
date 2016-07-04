64202ffd4d2cfbee7491ffadf332baf35e6d4c5c
KingsleyGu <gumin12150454x@gmail.com>
2016-07-04 09:47:37 +0200

    change the edit thread

--- include/staff/edit_thread.inc.php
+++ include/staff/edit_thread.inc.php
@@ -1,7 +1,20 @@
+<?php
+    $threadContent = "";
+    if(isset($_REQUEST['thread_id'])&&is_numeric($_REQUEST['thread_id']))
+    {
+        $thread = ThreadEntry::lookup($_REQUEST['thread_id']);
+        $threadContent = $thread->getMessage();
+    }
+
+?>
+
+
+
 <form method="POST" action="/scp/uploadFilesToThread.php">
     <?php csrf_token(); ?>
     <input type="hidden" name="ticket_id" value="<?php echo $_REQUEST['ticket_id'];?>">
     <input type="hidden" name="thread_id" value="<?php echo $_REQUEST['thread_id'];?>">
+    <textarea></textarea>
     <div id="reply_form_attachments" class="attachments">
     <?php
     print $response_form->getField('attachments')->render();
