1419da0ace8949ebcbbf4cefbf9ec5aee0e2b92a
KingsleyGu <gumin12150454x@gmail.com>
2016-07-05 15:35:30 +0200

    make the internal note more obvious

--- include/staff/edit_thread.inc.php
+++ include/staff/edit_thread.inc.php
@@ -14,8 +14,9 @@
     <?php csrf_token(); ?>
     <input type="hidden" name="ticket_id" value="<?php echo $_REQUEST['ticket_id'];?>">
     <input type="hidden" name="thread_id" value="<?php echo $_REQUEST['thread_id'];?>">
-
-    <?php echo $threadContent; ?><br>
+    <label>Original Content:</label>
+    <?php echo $threadContent; ?><br><br><br>
+    <label>Input the new content:</label>
     <textarea name="thread_content" style="width:100%;height:250px;"></textarea>
 
     <div id="reply_form_attachments" class="attachments">
