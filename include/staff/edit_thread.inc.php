c39e8fa4bb034170b09890e3acd4973959a33204
KingsleyGu <gumin12150454x@gmail.com>
2016-08-16 14:05:42 +0200

    make the searching box not far from each other

--- include/staff/edit_thread.inc.php
+++ include/staff/edit_thread.inc.php
@@ -15,7 +15,7 @@
     <input type="hidden" name="ticket_id" value="<?php echo $_REQUEST['ticket_id'];?>">
     <input type="hidden" name="thread_id" value="<?php echo $_REQUEST['thread_id'];?>">
     <label><b>Original Content:</b></label><br><hr>
-    <?php echo $threadContent; ?><br>
+    <?php echo $threadContent; ?><br><hr>
     <label><b>Input the new content:</b></label>
     <textarea name="thread_content" style="width:100%;height:150px;"></textarea>
 
