fed086cc914e1be6b4f898bd2fca10a8856bd131
KingsleyGu <gumin12150454x@gmail.com>
2016-08-16 12:55:16 +0200

    make the searching box not far from each other

--- include/staff/edit_thread.inc.php
+++ include/staff/edit_thread.inc.php
@@ -15,9 +15,9 @@
     <input type="hidden" name="ticket_id" value="<?php echo $_REQUEST['ticket_id'];?>">
     <input type="hidden" name="thread_id" value="<?php echo $_REQUEST['thread_id'];?>">
     <label><b>Original Content:</b></label><br>
-    <?php echo $threadContent; ?><br><br><br>
+    <?php echo $threadContent; ?><br>
     <label><b>Input the new content:</b></label>
-    <textarea name="thread_content" style="width:100%;height:250px;"></textarea>
+    <textarea name="thread_content" style="width:100%;height:150px;"></textarea>
 
     <div id="reply_form_attachments" class="attachments">
     <?php
