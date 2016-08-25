64ae50be962f66e33e05f22ccfc54a3ea3cdcc7a
KingsleyGu <gumin12150454x@gmail.com>
2016-08-25 09:21:04 +0200

    delete all the useless language files

--- include/i18n/lang/MANIFEST.php
+++
@@ -1,8 +0,0 @@
-<?php return array (
-  'Build-Date' => 'Wed, 19 Aug 15 16:52:18 -0500',
-  'Build-Version' => 'v1.9.12',
-  'Language' => 'da_DK',
-  'Id' => 'lang:da',
-  'Last-Revision' => '2015-06-23 16:56-0400',
-  'Version' => 143509,
-);
